c3dcbd41f545ef58fbe1f16ab1eadbbab49ea731
Manas Vijaywargiya <manasdevslopes@gmail.com>
2021-05-02 13:12:41 +0530

    Initial Commit

---
+++ Shared/Everyday_NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Everyday_NotesApp.swift
+//  Shared
+//
+//  Created by MANAS VIJAYWARGIYA on 02/05/21.
+//
+
+import SwiftUI
+
+@main
+struct Everyday_NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
